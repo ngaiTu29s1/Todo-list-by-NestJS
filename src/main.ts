0a4ebde0d9c0f6cfcaabbcd6c48c872bee7a0431
ngaiTu29s1 <trantuantu2004@gmail.com>
2025-07-07 16:16:08 +0700

    Fix: set default port to fit with render

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
